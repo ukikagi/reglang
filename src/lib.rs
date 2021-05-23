aef2f52be3443d3379f5040e2fbac9dc82ca98bb
ukikagi <ukikagi@gmail.com>
2021-05-23 20:39:14 +0900

    Implement parser

--- src/lib.rs
+++ src/lib.rs
@@ -1,3 +1,13 @@
+use nom::branch::alt;
+use nom::bytes::complete::tag;
+use nom::character::complete::char;
+use nom::character::complete::satisfy;
+use nom::combinator::map;
+use nom::combinator::value;
+use nom::sequence::delimited;
+use nom::sequence::pair;
+use nom::sequence::separated_pair;
+use nom::sequence::terminated;
 use nom::IResult;
 
 #[derive(Debug, Clone, PartialEq, Eq, Hash)]
@@ -17,11 +27,11 @@ fn literal(x: char) -> RegExp {
   RegExp::Literal(x)
 }
 
-fn concat(left: RegExp, right: RegExp) -> RegExp {
+fn concat((left, right): (RegExp, RegExp)) -> RegExp {
   RegExp::Concat(Box::new(left), Box::new(right))
 }
 
-fn union(left: RegExp, right: RegExp) -> RegExp {
+fn union((left, right): (RegExp, RegExp)) -> RegExp {
   RegExp::Union(Box::new(left), Box::new(right))
 }
 
@@ -29,8 +39,57 @@ fn star(re: RegExp) -> RegExp {
   RegExp::Star(Box::new(re))
 }
 
-fn parse(source: &str) -> IResult<&str, RegExp> {
-  Ok((source, empty()))
+fn regexp(input: &str) -> IResult<&str, RegExp> {
+  unionexp(input)
+}
+
+fn unionexp(input: &str) -> IResult<&str, RegExp> {
+  alt((
+    map(separated_pair(interexp, tag("|"), unionexp), union),
+    interexp,
+  ))(input)
+}
+
+fn interexp(input: &str) -> IResult<&str, RegExp> {
+  // TODO: implement intersection
+  concatexp(input)
+}
+
+fn concatexp(input: &str) -> IResult<&str, RegExp> {
+  alt((map(pair(repeatexp, concatexp), concat), repeatexp))(input)
+}
+
+fn repeatexp(input: &str) -> IResult<&str, RegExp> {
+  // Fow now a pattern like a+* is prohibited unlike dk.brics.automaton for simplicity
+  // cf. https://www.brics.dk/automaton/doc/dk/brics/automaton/RegExp.html
+  alt((map(terminated(complexp, tag("*")), star), complexp))(input)
+}
+
+fn complexp(input: &str) -> IResult<&str, RegExp> {
+  // TODO: implement complement
+  charclassexp(input)
+}
+
+fn charclassexp(input: &str) -> IResult<&str, RegExp> {
+  // TODO: implement charactor class
+  simpleexp(input)
+}
+
+fn simpleexp(input: &str) -> IResult<&str, RegExp> {
+  // TODO: implement other basic constructions e.g.
+  // . : any single charactor
+  // @ : any string
+  // () : empty string
+  alt((
+    charexp,
+    value(empty(), char('#')),
+    delimited(tag("("), unionexp, tag(")")),
+  ))(input)
+}
+
+fn charexp(input: &str) -> IResult<&str, RegExp> {
+  // TODO: support more characters including escaped ones
+  map(satisfy(|c: char| c.is_ascii_lowercase()), literal)(input)
 }
 
 #[cfg(test)]
@@ -42,33 +101,33 @@ mod tests {
   }
 
   #[test]
-  fn test_empty_set() {
-    assert_eq!(parse("#"), fully_parsed(empty()));
+  fn test_empty() {
+    assert_eq!(simpleexp("#"), fully_parsed(empty()));
   }
 
   #[test]
   fn test_literal() {
-    assert_eq!(parse("#"), fully_parsed(literal('a')));
+    assert_eq!(simpleexp("a"), fully_parsed(literal('a')));
   }
 
   #[test]
   fn test_concat() {
     assert_eq!(
-      parse("ab"),
-      fully_parsed(concat(literal('a'), literal('b')))
+      concatexp("ab"),
+      fully_parsed(concat((literal('a'), literal('b'))))
     );
   }
 
   #[test]
   fn test_union() {
     assert_eq!(
-      parse("a|b"),
-      fully_parsed(union(literal('a'), literal('b')))
+      unionexp("a|b"),
+      fully_parsed(union((literal('a'), literal('b'))))
     );
   }
 
   #[test]
   fn test_star() {
-    assert_eq!(parse("a*"), fully_parsed(star(literal('a'))));
+    assert_eq!(repeatexp("a*"), fully_parsed(star(literal('a'))));
   }
 }
